b7a7a753ed0d320dc69116dcabb51ac3c68a48d3
Alejandro Moreno <jose.alejandro.mr@gmail.com>
2024-05-02 22:08:15 -0700

    Fixing docker compose initialization scripts.

--- web/src/components/Notifications/Notifications.tsx
+++ web/src/components/Notifications/Notifications.tsx
@@ -23,6 +23,26 @@ function Notifications() {
   return (
     <>
       <h3>Notifications log</h3>
+      <div role="alert" className="alert alert-info">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="stroke-current shrink-0 w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          ></path>
+        </svg>
+        <span>
+          If a records are missing on the notification log try refreshing the
+          page (No real time updates implemented), it may take a couple of
+          seconds for the pubsub queue to be processed completely.
+        </span>
+      </div>
       <div className="overflow-x-auto">
         <table className="table table-sm table-pin-rows table-pin-cols border-spacing-2">
           <thead>
